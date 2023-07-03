1ad8aedd8f3e7c44cb0c13a419aa34b7e38a688c
Oana <oana.demian@gmail.com>
2023-07-03 17:16:47 +0100

    returns zero for an empty word

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,16 @@
 public class Scrabble {
+    private static String word = "";
+    public Scrabble (String word) {
+        this.word = word;
+    }
+    public static void main(String[] args) {
+        Scrabble.score();
+    }
+    public static int score() {
+        if (Scrabble.isEmptyWord()) return 0;
+        return 4;
+    }
+    public static boolean isEmptyWord () {
+        return (Scrabble.word == "");
+    }
 }
