274482c16378d9c7a60b4ac9bfabb81e18da21c1
Oana <oana.demian@gmail.com>
2023-07-08 13:40:43 +0100

    calculates score for double word

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,18 +1,46 @@
+import java.lang.reflect.Constructor;
 
 public class Scrabble {
-    protected String word;
+    private static String word;
+    private static Character[] firstL;
+    private static Character[] secondL;
+    private static boolean doubleWord;
+    private static boolean tripleWord;
+
+    private boolean justWord;
 
     public Scrabble(String wordToScore) {
         this.word = wordToScore;
+        this.justWord = true;
+    }
+
+    public Scrabble(String word, Character[] firstL, Character[] secondL, boolean doubleWord, boolean tripleWord) {
+        this.word = word;
+        this.firstL = firstL;
+        this.secondL = secondL;
+        this.doubleWord = doubleWord;
+        this.tripleWord = tripleWord;
+        this.justWord = false;
     }
 
     public static void main(String[] args) {
+//        Scrabble scrabble = new Scrabble("quirky", new Character[]{}, new Character[]{}, true, false);
+//        WordPlayed wordPlayed1 = new WordPlayed("quirky", new Character[]{}, new Character[]{}, true, false);
+//        System.out.println(wordPlayed.isDoubleWord("quirky"));
+//        System.out.println(wordPlayed.calculateWordScore("quirky"));
+//        System.out.println(scrabble.score());
 
     }
 
     public int score() {
-        WordPlayed wordPlayed = new WordPlayed(this.word);
-        return (wordPlayed.isNull() || wordPlayed.isEmptyWord()) ? 0 : wordPlayed.calculateWordScore(this.word);
+        WordPlayed wordPlayed1 = new WordPlayed(this.word);
+        WordPlayed wordPlayed2 = new WordPlayed(this.word, this.firstL, this.secondL, this.doubleWord, this.tripleWord);
+        if (this.justWord) {
+            return wordPlayed1.calculateWordScore(this.word);
+        }
+        return wordPlayed2.calculateWordScore(this.word);
     }
 
+
 }
+
--- src/main/java/WordPlayed.java
+++ src/main/java/WordPlayed.java
@@ -1,20 +1,38 @@
 public class WordPlayed {
-    private final String wordPlayed;
-    int finalScore;
+    private String wordPlayed;
+    private Character[] firstL;
+    private Character[] secondL;
+    private boolean doubleWord;
+    private boolean tripleWord;
+    private int finalScore;
 
     public WordPlayed(String wordPlayed) {
         this.wordPlayed = wordPlayed;
         this.finalScore = 0;
     }
 
+    public WordPlayed(String wordPlayed, Character[] firstL, Character[] secondL, boolean doubleWord, boolean tripleWord) {
+        this.wordPlayed = wordPlayed;
+        this.firstL = firstL;
+        this.secondL = secondL;
+        this.doubleWord = doubleWord;
+        this.tripleWord = tripleWord;
+        this.finalScore = 0;
+    }
+
+
     public char [] wordToLowerCaseLettersArray (String word){
         return (word.toLowerCase()).toCharArray();
     }
     public int calculateWordScore (String word) {
+        if (isNull() || isEmptyWord()) {
+            return 0;
+        }
         for (char letter : wordToLowerCaseLettersArray(word)) {
             this.finalScore += calculateLetterScore(letter);
         }
-        return this.finalScore;
+        return this.isDoubleWord(word) ? this.finalScore *= 2 : this.finalScore;
+
     }
     public int calculateLetterScore (char letter) {
         return isLetterValid(letter) ? getLetterScore (letter) : 0;
@@ -23,12 +41,20 @@ public class WordPlayed {
     public int getLetterScore (char letter) {
         return LetterValues.getLetterPoints().get(String.valueOf(letter));
     }
+
+    public boolean isDoubleWord(String word) {
+        return this.doubleWord;
+    }
+    public boolean isTripleWordWord(String word) {
+        return this.tripleWord;
+    }
     public boolean isLetterValid(char letter) {
         return LetterValues.getLetterPoints().containsKey(String.valueOf(letter));
     }
     public boolean isEmptyWord () {
         return (this.wordPlayed.equals(""));
     }
+
 
     public boolean isNull () {
         return (this.wordPlayed == null);
