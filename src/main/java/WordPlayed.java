10c09e2a4b8823725432ec9cc04ef6a9351854a1
Oana <oana.demian@gmail.com>
2023-07-09 13:57:12 +0100

    calculates score for triple letters

--- src/main/java/WordPlayed.java
+++ src/main/java/WordPlayed.java
@@ -40,43 +40,13 @@ public class WordPlayed {
         for (char doubleLetter : getDoubleLetters()) {
             this.finalScore += calculateLetterScore(doubleLetter);
         }
+
+        for (char tripleLetter : getTripleLetters()) {
+            this.finalScore += 2 * calculateLetterScore(tripleLetter);
+        }
         if (this.isDoubleWord(word)) { this.finalScore *= 2;};
         if (this.isTripleWord(word)) { this.finalScore *= 3;};
         return this.finalScore;
-    }
-
-    public static char[] removeArrayElement(char[] arr, int index)
-    {
-
-        // If the array is empty
-        // or the index is not in array range
-        // return the original array
-        if (arr == null || index < 0
-                || index >= arr.length) {
-
-            return arr;
-        }
-
-        // Create another array of size one less
-        char[] anotherArray = new char[arr.length - 1];
-
-        // Copy the elements except the index
-        // from original array to the other array
-        for (int i = 0, k = 0; i < arr.length; i++) {
-
-            // if the index is
-            // the removal element index
-            if (i == index) {
-                continue;
-            }
-
-            // if the index is not
-            // the removal element index
-            anotherArray[k++] = arr[i];
-        }
-
-        // return the resultant array
-        return anotherArray;
     }
     public int calculateLetterScore (char letter) {
         if (isLetterValid(letter)) {return getLetterScore(letter);};
@@ -91,35 +61,17 @@ public class WordPlayed {
         return dLettersArr;
     }
 
-//    public int premiumLetterScore(String word) {
-//        for (char letter : wordToLowerCaseLettersArray(word)) {
-//            for (char doubleLetter : getDoubleLetters()) {
-//                if (letter == doubleLetter) {
-//
-//                }
-//            }
-//        }
-//    }
+    public char[] getTripleLetters () {
+        String tLettersStr = Arrays.toString(this.tripleLetters);
+        char[] tLettersArr = tLettersStr.toLowerCase().toCharArray();
+        return tLettersArr;
+    }
 
-//    public boolean isTripleLetter (char letter) {
-//        char [] arr = this.wordPlayed.toCharArray();
-//        String str = String.valueOf(letterToLowerCase(letter));
-//        for (char c : arr) {
-//            if (c==letter) { return this.tripleLetters.toString().contains(str);}
-//        }
-//        return false;
-//    }
     public boolean isDoubleWord(String word) {
         return this.doubleWord;
     }
     public boolean isTripleWord(String word) {
         return this.tripleWord;
-    }
-
-    public char letterToLowerCase(char letter) {
-        String str = String.valueOf(letter).toLowerCase();
-        letter = str.charAt(0);
-        return letter;
     }
     public boolean isLetterValid(char letter) {
         return LetterValues.getLetterPoints().containsKey(String.valueOf(letter));
